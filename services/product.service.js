03a331ae5f77ba718788995aba9a3941ff358cb9
huynhgiathien <huynhgiathien1314@gmail.com>
2022-06-23 16:19:20 +0700

    Update product.service.js

--- services/product.service.js
+++ services/product.service.js
@@ -22,8 +22,8 @@ module.exports = class ProductService extends BaseService {
       files.map((file) => {
         // let img = fs.readFileSync(file.path)
         console.log('this is path', file.destination)
-        const path = file.destination.split('server\\')[1]
-        const newPath = path.split('\\').join('/')
+        // const path = file.destination.split('server\\')[1]
+        // const newPath = path.split('\\').join('/')
         imageList.push(file.destination + "/" + file.filename)
       })
       const product = {cate_id, mate_id, name, price, unitsinstock, description, imageList}
