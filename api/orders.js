5ecd429016ab20ce16b5cd5b9d8400ab97e12ba3
huynhgiathien <huynhgiathien1314@gmail.com>
2022-07-02 17:08:38 +0700

    Update API

--- api/orders.js
+++ api/orders.js
@@ -8,12 +8,12 @@ const ShipFeeService = require('../services/shipFee.service').getInstance()
 const Product = require('../models/product.model.js')
 const Order = require('../models/order.model.js')
 const OrderDetail = require('../models/orderDetail.model.js')
-const OrderService = require('../services/order.service')
+const OrderService = require('../services/order.service').getInstance()
 
 module.exports = (router) => { 
   router.post('/create', async (req, res, next) => {
 		try {
-			const result = await OrderService.create(req.body)
+			const result = await OrderDetail.create(req.body)
 			if (result.is_completed) {
 				return res.status(200).json({msg:result.message})
 			}
@@ -24,4 +24,32 @@ module.exports = (router) => {
 			return res.status(500).json(err.message)
 		}
   })
+	// router.get('/', async (req, res, next) => {
+	// 	try {
+  //     const page = parseInt(req.query.page);
+  //     const size = parseInt(req.query.size);
+  //     const cate_id = req.query.cate_id?.split(",");
+  //     const mate_id = req.query.mate_id?.split(",");
+  //     const { order_by, name, sort, direction } = req.query;
+  //     const is_instock = req.query.is_instock?.split(",")
+  //     const result = await ProductService.get({
+  //       order_by,
+  //       is_instock,
+  //       mate_id,
+  //       cate_id,
+  //       name,
+  //       page,
+  //       size,
+  //       sort,
+  //       direction,
+  //     });
+  //     return res.status(200).json({
+  //       data: result,
+  //     });
+	// 	}
+	// 	catch (err) {
+	// 		console.log(err.message)
+	// 		return res.status(500).json(err.message)
+	// 	}
+  // })
 }
