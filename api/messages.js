f1a7ab2e6eb026e10d6bb2b0fc8ca86c57a03620
huynhgiathien <48886236+huynhgiathien@users.noreply.github.com>
2022-07-17 14:44:38 +0700

    Update messages.js

--- api/messages.js
+++ api/messages.js
@@ -1,45 +1,45 @@
-// const validator = require('../middlewares/validator')
-// const MessageServices = require('../services/message.service').getInstance()
-// const ProductService = require('../services/product.service').getInstance()
-// const authorize = require("../middlewares/authorize.js");
+const validator = require('../middlewares/validator')
+const MessageServices = require('../services/message.service').getInstance()
+const ProductService = require('../services/product.service').getInstance()
+const authorize = require("../middlewares/authorize.js");
 
 
-// module.exports = (router) => { 
-//   router.get('/', authorize.verifyAccessToken,
-//     async (req, res, next) => {
-//       try {
-//         const userId = req.payload._id
-//         const message = await MessageServices.getMessagesByCustomerId(userId)
-//         return res.status(200).json({ 
-//           message
-//         })
-//       }
-//       catch(error) {
-// 				console.log(error)
-//         next(error);
-//       }
-//   }),
+module.exports = (router) => { 
+  router.get('/', authorize.verifyAccessToken,
+    async (req, res, next) => {
+      try {
+        const userId = req.payload._id
+        const message = await MessageServices.getMessagesByCustomerId(userId)
+        return res.status(200).json({ 
+          message
+        })
+      }
+      catch(error) {
+				console.log(error)
+        next(error);
+      }
+  }),
 
-//   router.get('/:id', authorize.verifyAccessToken,
-//   async (req, res, next) => {
-//     try {
-//       const type = req.payload.type
-//       if (type === 2 || type === 3 ) {
-//         const id = req.params.id
-//         const message = await MessageServices.getMessagesByCustomerId(id)
-//         return res.status(200).json({ 
-//           message
-//         })
-//       }
-//       else {
-//         return res.status(400).json({ 
-//           msg: "Bạn không có quyền truy cập!!!"
-//         })
-//       }
-//     }
-//     catch(error) {
-//       console.log(error)
-//       next(error);
-//     }
-// })
-// }
+  router.get('/:id', authorize.verifyAccessToken,
+  async (req, res, next) => {
+    try {
+      const type = req.payload.type
+      if (type === 2 || type === 3 ) {
+        const id = req.params.id
+        const message = await MessageServices.getMessagesByCustomerId(id)
+        return res.status(200).json({ 
+          message
+        })
+      }
+      else {
+        return res.status(400).json({ 
+          msg: "Bạn không có quyền truy cập!!!"
+        })
+      }
+    }
+    catch(error) {
+      console.log(error)
+      next(error);
+    }
+})
+}
